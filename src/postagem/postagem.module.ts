1039b9045b9c258487243558c28d5856cb7a9623
Beatriz <alvesbeatriz148@gmail.com>
2024-04-18 11:56:17 -0300

    fix: correção na relação entre as tabelas

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Postagem } from "./entities/postagem.entity";
 import { PostagemService } from "./services/postagem.service";
 import { PostagemController } from "./controllers/postagem.controller";
+import { TemaModule } from "src/tema/tema.module";
+import { TemaService } from "src/tema/services/tema.service";
 
 // classe principal do módulo, onde seram registrada todas as classes
 @Module({
-    imports: [TypeOrmModule.forFeature([Postagem])],
-    providers: [PostagemService],
+    imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
+    providers: [PostagemService, TemaService],
     controllers: [PostagemController],
     exports: [TypeOrmModule]
 })
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -2,13 +2,15 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
+import { TemaService } from "src/tema/services/tema.service";
 
 @Injectable() //pode ser injetada em outras classes
 export class PostagemService {
     constructor(
         @InjectRepository(Postagem)
         //objeto responsável por implementar a injeção de dependências
-        private postagemRepository: Repository<Postagem>
+        private postagemRepository: Repository<Postagem>,
+        private temaService: TemaService
     ) {}
 
     async findAll(): Promise<Postagem[]> {
@@ -37,6 +39,13 @@ export class PostagemService {
     }
 
     async create(postagem: Postagem): Promise<Postagem> {
+        if(postagem.tema) {
+            let tema = await this.temaService.findById(postagem.tema.id)
+            if(!tema) {
+                throw new HttpException('Tema não encontrado!', HttpStatus.NOT_FOUND);
+            }
+            return await this.postagemRepository.save(postagem);
+        }
         return await this.postagemRepository.save(postagem);
     }
 
@@ -44,6 +53,13 @@ export class PostagemService {
         let buscaPostagem: Postagem = await this.findById(postagem.id);
         if(!buscaPostagem || !postagem.id) {
             throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+        }
+        if(postagem.tema) {
+            let tema = await this.temaService.findById(postagem.tema.id)
+            if(!tema) {
+                throw new HttpException('Tema não encontrado!', HttpStatus.NOT_FOUND);
+            }
+            return await this.postagemRepository.save(postagem);
         }
         return await this.postagemRepository.save(postagem);
     }
