ecb0a9ecd13104bfb7105333065939cb422ae985
Beatriz <alvesbeatriz148@gmail.com>
2024-05-02 13:34:40 -0300

    feat: deploy do projeto

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe()); //checará todas as requisições
   app.enableCors(); //permite receber requicisão http de outras origens
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
